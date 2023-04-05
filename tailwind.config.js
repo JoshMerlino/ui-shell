845881c41d36c2383be2e14a9d05280c3a8fba03
Josh Merlino <joshua.merlino@gmail.com>
2023-04-05 04:04:52 +0000

    add primary alias

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
 export default {
 	content: [
 		"./index.html",
 		"./app/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary": { ...colors.blue, DEFAULT: colors.blue[500] }
+			}
+		},
 	},
 	plugins: [],
 };
