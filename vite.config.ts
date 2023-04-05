a6065a096c0bdc326f7b3fd418b50eb291e6c946
Josh Merlino <joshua.merlino@gmail.com>
2023-04-05 03:59:08 +0000

    move stuff around

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [ react() ],
+	root: "app",
+	publicDir: "static",
+	build: {
+		outDir: "../public_html",
+		emptyOutDir: true,
+	},
+	server: {
+		port: 8080,
+		hmr: {
+			protocol: "ws"
+		}
+	}
+});
